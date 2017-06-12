25cc26b11b5c551567667c3f0d7def24eea19bd2
navomij <jason.kadarusman@navomi.com>
2017-06-11 14:38:12 -1000

    MultiAccount Trader Website

---
+++ client/imports/app/pages/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by jasonk on 5/6/17.
+ */
+import { HomePage } from './home/home.component';
+import { BrokerLatencyPage } from './broker-latency/broker-latency.component';
+import { AboutUsPage } from '../pages/about-us/about-us.component';
+import { FreeVPSPage } from '../pages/free-vps/free-vps.component';
+import { MultiaccountPage } from '../pages/multiaccount/multiaccount.component';
+// More pages
+import { ContactPage } from '../pages/contact/contact.component';
+import { FAQPage } from '../pages/faq/faq.component';
+import { KnowledgeBasePage } from '../pages/knowledge-base/knowledge-base.component';
+import { TermsConditionsPage } from '../pages/terms-conditions/terms-conditions.component';
+// Login and Register Pages
+import { LoginPage } from '../pages/login/login.component';
+import { RegisterPage } from '../pages/register/register.component';
+
+export const PAGES_DECLARATIONS = [
+    HomePage, BrokerLatencyPage, AboutUsPage, FreeVPSPage, MultiaccountPage,
+    ContactPage, FAQPage, KnowledgeBasePage, TermsConditionsPage,
+    LoginPage, RegisterPage
+];
---
+++ client/imports/app/pages/multiaccount/multiaccount.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by jasonk on 6/11/17.
+ */
+import { Component } from '@angular/core';
+import template from "./multiaccount.html";
+
+@Component({
+    selector: 'multiaccount-page',
+    template
+})
+export class MultiaccountPage {}
